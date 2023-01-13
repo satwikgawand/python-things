d4426639939b9803aa49e21ffc566e582ce06470
Satwik Gawand <satwikgawand@gmail.com>
2023-01-13 23:36:57 +0800

    add: header script

---
+++ 1-python-file-headers/script.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Docstring
+
+"""
+
+__author__ = "Satwik Gawand"
+__license__ = "MIT License"
+__version__ = "0.0.1"
+__maintainer__ = "Satwik Gawand"
+
+# --- your code below this line --- #
